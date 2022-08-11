94adc551e5b0e409ddb754b45bf2801a1aab2671
Yu Jung Eun <0hh.0553@gmail.com>
2022-08-11 13:08:19 +0900

    lotto 최대 숫자 변경

--- python_vacation/Lotto_2407.py
+++ python_vacation/Lotto_2407.py
@@ -1,16 +1,19 @@
 # 2407 유정은
 
 import random
+
+
 def func_lotto():
     li = list()
 
     for i in range(6):
-        n = random.randint(1,20)
+        n = random.randint(1, 45)
         while n in li:
-            n = random.randint(1,20)
+            n = random.randint(1, 45)
         li.append(n)
 
     return li
+
 
 for i in range(10):
     li = func_lotto()
@@ -19,5 +22,5 @@ for i in range(10):
     print("당첨번호 : ", end='')
     for i in range(len(li)):
         print(li[i], end=' ')
-    
+
     print()
